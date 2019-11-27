48e673b0c8329ce61a3dbe0bbe17c4be86c2e579
Ramon96 <rfm1@live.nl>
2019-11-27 17:37:37 +0100

    fixed the bars

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import { getQuery } from './scripts/getData';
-import { 
-  select, 
-  scaleLinear, 
-  max, 
-  scaleBand, 
-  axisLeft, 
-  axisBottom, 
+import {
+  getQuery
+} from './scripts/getData';
+import {
+  select,
+  scaleLinear,
+  max,
+  scaleBand,
+  axisLeft,
+  axisBottom,
   json,
   tsv,
   geoPath,
@@ -19,7 +21,9 @@ import * as d3 from 'd3';
 import d3Tip from 'd3-tip';
 d3.tip = d3Tip;
 
-import { feature } from 'topojson';
+import {
+  feature
+} from 'topojson';
 
 
 
@@ -48,7 +52,7 @@ SELECT ?title ?typeLabel ?long ?lat ?plaats (SAMPLE(?cho) as ?filtered)  (COUNT(
 
 
 
-getQuery(endpoint, countQuery).then(data =>{
+getQuery(endpoint, countQuery).then(data => {
 
 
   init(data)
@@ -56,151 +60,208 @@ getQuery(endpoint, countQuery).then(data =>{
 
 
 
-function init(data){
+function init(data) {
   const maskData = d3.nest()
-  .key(function(d) { return d.typeLabel; })
-  .entries(data);
+    .key(function (d) {
+      return d.typeLabel;
+    })
+    .entries(data);
 
-// console.log(data[0].typeLabel)
 
-// map
+  const items = data.map(index => index.typeLabel);
+  const colorscale = d3.scaleOrdinal()
+    .domain(items)
+    .range(d3.schemeCategory10);
+    console.log(items)
+  // console.log(data[0].typeLabel)
+
+  // map
   const svgMap = select('.map');
   const projection = geoNaturalEarth1();
   const pathGenerator = geoPath().projection(projection);
   const gMap = svgMap.append('g');
 
   // barChart 
+
+
+  createBarchart(data);
+
+
+  function createBarchart(data) {
+    const newData = d3.nest()
+      .key(d => d.typeLabel)
+      .rollup(d => {
+        return {
+          amount: d.length
+        }
+      }).entries(data)
+
     const barTooltip = d3.tip().attr('class', 'd3-tip').html(function (d) {
-      const returnValue = d.typeLabel + ": " + d.count;
-      return returnValue ;
+      const returnValue = d.key + ": " + d.value.amount;
+      return returnValue;
     });
-  const svgBar = select('.barChart')
-  .call(barTooltip);
-  const width = +svgBar.attr("width");
-  const height = +svgBar.attr("height");
+    
 
-  // console.log(d)
-  const xValue = d => +d.count;
-  const yValue = d=> d.typeLabel;
+    const svgBar = select('.barChart')
+      .call(barTooltip);
+    const width = +svgBar.attr("width");
+    const height = +svgBar.attr("height");
+
+    // console.log(d)
+    const xValue = d => +d.value.amount;
+    const yValue = d => d.key;
+
+    const margin = {
+      top: 20,
+      right: 100,
+      left: 180,
+      bottom: 30
+    };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    const xScale = scaleLinear()
+      .domain([0, max(newData, xValue)])
+      .range([0, innerWidth])
+
+    const yScale = scaleBand()
+      .domain(newData.map(yValue))
+      .range([0, innerHeight])
+      .padding(0.1)
 
 
-  const margin = {top: 20, right : 100, left: 180, bottom: 30};
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
-  const xScale = scaleLinear()
-  	.domain([0, max(data, xValue)])
-  	.range([0, innerWidth])
-  
-  const yScale = scaleBand()
-  	.domain(data.map(yValue))
-  	.range([0, innerHeight])
-  	.padding(0.1)
-  
-  const items = data.map(index => index.typeLabel);
-  const colorscale = d3.scaleOrdinal()
-  .domain(items)
-  .range(d3.schemeCategory10);
-  
-  const g = svgBar.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`)
-  
-  g.append('g').call(axisLeft(yScale));
-  g.append('g').call(axisBottom(xScale))
-  .attr('transform', `translate(0, ${innerHeight})`)
 
-  	
-	g.selectAll('rect')
-  	.data(data)
-  	.enter()
-  	.append('rect')
-  	.attr('y', d => yScale(yValue(d)))
-  	.attr('width', d => xScale(xValue(d)))
-    .attr('height', yScale.bandwidth())
-    .style('fill', function(d){
-      return colorscale(d.typeLabel)
-    })
-    .on('mouseenter', barTooltip.show)
-    .on('mouseleave', barTooltip.hide)
-    .on('click', function(d){hightlightDatapoint(d.typeLabel)})
-  	.append('title')
+    const g = svgBar.append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+    g.append('g').call(axisLeft(yScale));
+    g.append('g').call(axisBottom(xScale))
+      .attr('transform', `translate(0, ${innerHeight})`)
+
+    console.log(newData)
+
+    g.selectAll('rect')
+      .data(newData)
+      .enter()
+      .append('rect')
+      .attr('y', d => yScale(yValue(d)))
+      .attr('width', d => xScale(xValue(d)))
+      .attr('height', yScale.bandwidth())
+      .style('fill', function (d) {
+        return colorscale(d.key)
+      })
+      .on('mouseenter', barTooltip.show)
+      .on('mouseleave', barTooltip.hide)
+      .on('click', function (d) {
+        hightlightDatapoint(d.key)
+      })
+      .append('title')
       .text(d => xValue(d))
 
-    
-  g.append('text')
-    .text('Hoeveelheid maskers per functie')
-    
-//map code
-svgMap.call(zoom().on('zoom', () => {
-  gMap.attr('transform', event.transform);
-}));
 
-const legend = svgMap.selectAll(".legend")
-.data(colorscale.domain())
-.enter().append("g")
-.attr("class", "legend")
-.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; })
+    g.append('text')
+      .text('Hoeveelheid maskers per functie')
+  }
+
+  //map code
+  svgMap.call(zoom().scaleExtent([0.9, 10]).on('zoom', () => {
+    gMap.attr('transform', event.transform);
+  }));
+
+  const legend = svgMap.selectAll(".legend")
+    .data(colorscale.domain())
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function (d, i) {
+      return "translate(0," + i * 20 + ")";
+    })
 
 
-legend.append("rect")
-.attr("x", 190 - 18)
-.attr("width", 18)
-.attr("height", 18)
-.style("fill", colorscale);
+  legend.append("rect")
+    .attr("x", 190 - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", colorscale);
 
-legend.append("text")
-.attr("x", 185 - 24)
-.attr("y", 9)
-.attr("dy", ".35em")
-.style("text-anchor", "end")
-.text(function(d) { return d; })
+  legend.append("text")
+    .attr("x", 185 - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function (d) {
+      return d;
+    })
 
   gMap.append('path')
-  .attr('class', 'sphere')
-  .attr('d', pathGenerator({type: 'Sphere'}))
+    .attr('class', 'sphere')
+    .attr('d', pathGenerator({
+      type: 'Sphere'
+    }))
 
   Promise.all([
     tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
     json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
   ]).then(([tsvData, topoJSONdata]) => {
-        const countryName = tsvData.reduce((accumulator, row) => {
-          accumulator[row.iso_n3] = row.name;
-            return accumulator;
-        }, {});
-    
-        const country = feature(topoJSONdata, topoJSONdata.objects.countries);
-          gMap.selectAll('path')
-            .data(country.features)
-            .enter()
-            .append('path')
-            .attr('class', 'country')
-            .attr('d', d => pathGenerator(d))
-          .append('title')
-            .text(d => countryName[d.id]);
-    
-      gMap.selectAll('circle')
-        .data(maskData[0].values)
-                .enter().append("circle")
-                .attr("transform", function(d) {
-                 //console.log(d.values.map(item => item.long))
-                  return "translate(" + projection([d.long, d.lat]) + ")";
-                })
-                .style('fill', function(d){
-                  return colorscale(d.typeLabel)
-                })
-                .transition()
-                    .delay(1000)
-                    .duration(700)
-                    .ease(d3.easeBounce)
-                .attr("r", 1)
+    const countryName = tsvData.reduce((accumulator, row) => {
+      accumulator[row.iso_n3] = row.name;
+      return accumulator;
+    }, {});
+
+    const country = feature(topoJSONdata, topoJSONdata.objects.countries);
+    gMap.selectAll('path')
+      .data(country.features)
+      .enter()
+      .append('path')
+      .attr('class', 'country')
+      .attr('d', d => pathGenerator(d))
+      .append('title')
+      .text(d => countryName[d.id]);
+
+    //plotPoints(maskData)
+
+
   });
 
-  function hightlightDatapoint(option){
-    const selection = maskData.filter(subject => subject.key == option)
-    console.log(selection[0].values)
+  function plotPoints(data) {
+    console.log(data)
+    const point = gMap.selectAll('circle');
+    point.data(data)
+      .attr("transform", function (d) {
+        //console.log(d.values.map(item => item.long))
+        return "translate(" + projection([d.long, d.lat]) + ")";
+      })
+      .style('fill', function (d) {
+        return colorscale(d.typeLabel)
+      })
+      .transition()
+      .delay(1000)
+      .duration(700)
+      .ease(d3.easeBounce)
+      .attr("r", 1);
+
+    point.data(data).enter()
+      .append("circle")
+      .attr("transform", function (d) {
+        //console.log(d.values.map(item => item.long))
+        return "translate(" + projection([d.long, d.lat]) + ")";
+      })
+      .style('fill', function (d) {
+        return colorscale(d.typeLabel)
+      })
+      .transition()
+      .delay(1000)
+      .duration(700)
+      .ease(d3.easeBounce)
+      .attr("r", 1);
+
+    point.data(data).exit().remove();
+  }
+
+  function hightlightDatapoint(option) {
+    const selection = maskData.filter(subject => subject.key == option).reduce(subject => subject)
+
+    plotPoints(selection.values)
+    console.log(selection.values)
+
   }
 
 }
-
-
-
-
